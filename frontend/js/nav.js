be47a1d3e5ff578bf051bec3d3ee8d80c5de1e52
Kabary <ahmedmohamedddd24@gmail.com>
2024-12-30 09:03:27 -0800

    Delete frontend directory

--- frontend/js/nav.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn');
-const mobileMenu = document.querySelector('.mobile-menu');
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-});
